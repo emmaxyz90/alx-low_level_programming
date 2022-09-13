338bffdfc58411171be273b3ffcd73075859e4a8
emmaxyz90 <oniemmanuel90@gmail.com>
2022-09-13 06:09:37 -0700

    Adding 4-puts c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Retutn: Always 0 (Success)
+ */
+int main(void)
+{
+	char myString[] = "Programming is like building a multilingual puzzle";
+	puts(myString);
+
+	return(0);
+}
