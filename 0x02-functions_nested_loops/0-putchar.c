f648abcdfa546399f121d9e4f986bec9ee69a924
Oni Emmanuel <44571157+emmaxyz90@users.noreply.github.com>
2022-09-21 17:51:34 +0100

    Update 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char word[9] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+_putchar(word[i]);
+_putchar('\n');
+return (0);
+}
