4e49f862431a723793455404a862dd37944858d2
emmaxyz90 <oniemmanuel90@gmail.com>
2022-09-21 19:13:04 -0700

    Modified Issuper script

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,7 +5,7 @@
  * @c: int to be checked
  * Return: 1 if c is a digit, 0 otherwise
  */
-int _isdigit(int c)
+int _isupper(int c)
 {
-	return (c >= '0' && c <= '9');
+	return (c >= 'A' && c <= 'Z');
 }
