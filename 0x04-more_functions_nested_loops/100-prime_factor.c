54216c341e4650c3d3c46bcc66babee72316ef99
Oni Emmanuel <44571157+emmaxyz90@users.noreply.github.com>
2022-09-22 04:51:28 +0100

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,42 +1,27 @@
 #include <stdio.h>
-
+#include <math.h>
 /**
- * C program to find all prime factors of a given number
+ * main - main funcion
+ *
+ * Return: end program
  */
-
-int main()
+int main(void)
 {
-    int i, j, isPrime;
+	long int c, b, a = 612852475143;
 
-    /* Input a number from user */
-    long  num = 612852475143;
+	while (a != 1)
+	{
+		for (b = 2; b <= a; ++b)
+		{
+			if ((a % b) == 0)
+			{
+				break;
+			}
+		}
+		c = (a / b);
+		a = c;
+	}
+	printf("%li\n", b);
 
-    printf("All Prime Factors of %ld are: \n", num);
-
-    /* Find all Prime factors */
-    for(i=2; i<=num; i++)
-    {
-        /* Check 'i' for factor of num */
-        if(num%i==0)
-        {
-            /* Check 'i' for Prime */
-            isPrime = 1;
-            for(j=2; j<=i/2; j++)
-            {
-                if(i%j==0)
-                {
-                    isPrime = 0;
-                    break;
-                }
-            }
-
-            /* If 'i' is Prime number and factor of num */
-            if(isPrime==1)
-            {
-                printf("%d, ", i);
-            }
-        }
-    }
-
-    return 0;
+	return (0);
 }
